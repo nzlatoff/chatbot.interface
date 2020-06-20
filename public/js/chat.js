83913090c5c80c142efe7e5491daa25c96ddb1ca
jchwenger <34098722+jchwenger@users.noreply.github.com>
2020-06-20 16:01:00 +0100

    chat | cooking deletion function (commented), useful for reset

--- public/js/chat.js
+++ public/js/chat.js
@@ -6,6 +6,17 @@ var socket = io();
 
 var messages = document.getElementById("messages");
 
+// function deleteAllCookies() {
+//   var cookies = document.cookie.split(";");
+//   for (var i = 0; i < cookies.length; i++) {
+//     var cookie = cookies[i];
+//     var eqPos = cookie.indexOf("=");
+//     var name = eqPos > -1 ? cookie.substr(0, eqPos) : cookie;
+//     document.cookie = name + "=;expires=Thu, 01 Jan 1970 00:00:00 GMT";
+//   }
+// }
+// deleteAllCookies();
+
 // util function to parse local cookie
 function str_obj(str) {
   str = str.split('; ');
