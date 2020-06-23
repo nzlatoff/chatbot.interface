00ae56baa90324ec670ec56b60da816bbf85fe17
jchwenger <34098722+jchwenger@users.noreply.github.com>
2020-06-23 20:11:43 +0100

    db | really trying without

--- dbconnect.js
+++ dbconnect.js
@@ -1,8 +1,8 @@
-const mongoose = require("mongoose");
-mongoose.Promise = require("bluebird");
+// const mongoose = require("mongoose");
+// mongoose.Promise = require("bluebird");
 
-const url = "mongodb://localhost:27017/chat";
+// const url = "mongodb://localhost:27017/chat";
 
-const connect = mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true });
+// const connect = mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true });
 
-module.exports = connect;
+// module.exports = connect;
--- models/Chat.js
+++ models/Chat.js
@@ -1,23 +1,23 @@
-const mongoose = require("mongoose");
-const Schema = mongoose.Schema;
+// const mongoose = require("mongoose");
+// const Schema = mongoose.Schema;
 
-const chatSchema = new Schema(
-  {
-    character: {
-      type: String
-    },
-    message: {
-      type: String
-    },
-    sender: {
-      type: String
-    }
-  },
-  {
-    timestamps: true
-  }
-);
+// const chatSchema = new Schema(
+//   {
+//     character: {
+//       type: String
+//     },
+//     message: {
+//       type: String
+//     },
+//     sender: {
+//       type: String
+//     }
+//   },
+//   {
+//     timestamps: true
+//   }
+// );
 
-let Chat = mongoose.model("theChat", chatSchema);
+// let Chat = mongoose.model("theChat", chatSchema);
 
-module.exports = Chat;
+// module.exports = Chat;
