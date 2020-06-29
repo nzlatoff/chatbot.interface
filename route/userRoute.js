8b86d5a6fd5bb14bccc38d36513e02e345c771f6
jchwenger <34098722+jchwenger@users.noreply.github.com>
2020-06-29 22:56:04 +0100

    user route | deet

--- route/userRoute.js
+++ route/userRoute.js
@@ -7,9 +7,9 @@ const router = express.Router();
 router.route('/').get((req, res, next) => {
   res.setHeader('Content-Type', 'application/json');
   res.statusCode = 200;
-  var json = res.app.locals.clientsocketlist;
-  //console.log(json);
-  res.json(json);
+  const clientsocketlist = res.app.locals.clientsocketlist;
+  //console.log(clientsocketlist);
+  res.json(clientsocketlist);
 });
 
 module.exports = router;
