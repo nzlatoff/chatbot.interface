9c7fc8d43d7325a26c0d1363432de5eadfac097b
jchwenger <34098722+jchwenger@users.noreply.github.com>
2020-10-28 21:21:11 +0000

    entrails | console debug

--- public/js/entrails.js
+++ public/js/entrails.js
@@ -32,7 +32,7 @@ function createMessage(data) {
           i += 1;
           d.innerHTML += data.entrails;
           d.normalize();
-          // console.log(i, d.getClientRects(), d.getClientRects()[0].width, container.clientWidth);
+          console.log(i, d.getClientRects(), d.getClientRects()[0].width, container.clientWidth);
         } while (i < 2000 && d.getClientRects()[0].width < container.clientWidth - 5);
         d.innerHTML = d.innerHTML.substring(0, d.innerHTML.length) + "\n";
       } else {
